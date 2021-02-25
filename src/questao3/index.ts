0452578899743b0fb9efc332786497cb19623995
caio guilherme de lira dos santos <caioguilhermeuna@gmail.com>
2021-02-25 10:28:20 -0300

    Questoes 3, 4, 7, 10 feitas(falta testar)

---
+++ src/questao3/index.ts
@@ -0,0 +1,40 @@
+// 3.1
+
+const arr = [1,2,3,4,5];
+
+arr.map((item) => {
+    return item + 10;
+});
+
+//3.2
+
+const usuario1 = {
+    nome: 'Diego',
+    idade: 23
+};
+
+const mostraIdade = (usuario: any) => {
+    return usuario.idade;
+};
+
+mostraIdade(usuario1);
+
+//3.3
+
+const nome1 = 'Diego';
+const idade1 = 23;
+
+const mostraUsuario = (nome = 'Diego', idade = 18 ) => {
+    return { nome , idade };
+};
+
+mostraUsuario(nome1, idade1);
+mostraUsuario(nome1);
+
+//3.4
+
+const promise = () => {
+    return new Promise<void>((resolver, reject) => {
+        return resolver();
+    })
+}
---
+++ src/questao7/index.ts
@@ -0,0 +1,8 @@
+const nome2 = 'Diego';
+const idade2 = 23;
+
+const usuario3 = {
+    nome2,
+    idade2,
+    cidade: 'Rio do Sul',
+};
